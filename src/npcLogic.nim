36cbec3c884f060afb84c32b7ff5d66c1f953aa8
Scopsula <scopsula@tuta.com>
2026-04-29 22:30:29 +0100

    Update name

--- src/npcLogic.nim
+++ src/npcLogic.nim
@@ -1,6 +1,6 @@
 #[
 Rockman-X-Fossil-Hunter
-Copyright (C) 2026 Kyphosula
+Copyright (C) 2026 Sulabird
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
